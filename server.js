0639d389bc2e4772536fc527851019b638fe6ed8
Ranjit Paranjothi <ranjit@Ranjits-MacBook-Pro.local>
2021-08-04 15:55:09 +0530

    mongo DB setup done

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+connectDB();
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
